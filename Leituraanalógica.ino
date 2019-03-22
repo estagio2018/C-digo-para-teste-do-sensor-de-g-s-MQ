f12a8c7c5ddd7950f276f7e43c79eb49037e37db
estagio2018 <47534040+estagio2018@users.noreply.github.com>
2019-03-22 14:40:58 -0300

    Update Leituraanalógica.ino

--- Leituraanalógica.ino
+++ Leituraanalógica.ino
@@ -1,20 +1,23 @@
 const int MQ_PIN = A0;
-const int MQ_DELAY = 2000;
- 
+const int MQ_DELAY = 30000;
+
 void setup()
 {
   Serial.begin(9600);
 }
  
-void loop() 
+void loop()
 {
-  int raw_adc = analogRead(MQ_PIN);
-  float value_adc = raw_adc * (5.0 / 1023.0);
- 
-  Serial.print("Raw:");
-  Serial.print(raw_adc);
-  Serial.print("    Tension:");
-  Serial.println(value_adc);
- 
+  int analog = analogRead(MQ_PIN);
+  float VRL = analog * (5.0 / 1023.0);
+  float Rs = (5.0 - VRL) / VRL; 
+  Serial.print("analog:");
+  Serial.print(analog);
+  Serial.print("    VRL:");
+  Serial.print(VRL);
+   Serial.print("V");
+  Serial.print("    Rs:");
+  Serial.print(Rs);
+  Serial.println("kΩ");
   delay(MQ_DELAY);
 }
